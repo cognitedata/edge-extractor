16e4ba6f19cf5a1358158647e9768d90358db7c1
alivinco <aleksandrs.livincovs@gmail.com>
2023-11-05 12:22:19 +0100

    AES encryption v1

--- internal/config.go
+++ internal/config.go
@@ -1,6 +1,10 @@
 package internal
 
-import "github.com/cognitedata/cognite-sdk-go/pkg/cognite/dto/core"
+import (
+	"github.com/cognitedata/cognite-sdk-go/pkg/cognite/dto/core"
+)
+
+var Key = ""
 
 type StaticConfig struct {
 	ProjectName  string
@@ -18,4 +22,42 @@ type StaticConfig struct {
 	LogDir              string
 
 	LocalIntegrationConfig []core.Asset
+	IsEncrypted            bool
+}
+
+func (config *StaticConfig) Encrypt() error {
+	var err error
+	config.Secret, err = EncryptString(Key, config.Secret)
+	// iterate over LocalIntegrationConfig and encrypt all passwords
+	for i, _ := range config.LocalIntegrationConfig {
+		password, ok := config.LocalIntegrationConfig[i].Metadata["password"]
+		if ok {
+			config.LocalIntegrationConfig[i].Metadata["password"], err = EncryptString(Key, password)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	config.IsEncrypted = true
+	return err
+}
+
+func (config *StaticConfig) Decrypt() error {
+	var err error
+	if !config.IsEncrypted {
+		return nil
+	}
+	config.Secret, err = DecryptString(Key, config.Secret)
+	// iterate over LocalIntegrationConfig and decrypt all passwords
+	for i, _ := range config.LocalIntegrationConfig {
+		password, ok := config.LocalIntegrationConfig[i].Metadata["password"]
+		if ok {
+			config.LocalIntegrationConfig[i].Metadata["password"], err = DecryptString(Key, password)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	config.IsEncrypted = false
+	return err
 }
--- internal/utils.go
+++ internal/utils.go
@@ -1,7 +1,13 @@
 package internal
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -35,4 +41,65 @@ func GetBinaryDir() string {
 	}
 	filename := os.Args[1] // get command line first parameter
 	return filepath.Dir(filename)
+}
+
+func EncryptString(key, text string) (string, error) {
+	// Convert key to 16 bytes
+	keyBytes := []byte(key)
+	if len(keyBytes) != 16 {
+		return "", errors.New("key must be 16 bytes")
+	}
+
+	// Convert text to bytes
+	textBytes := []byte(text)
+
+	// Generate a new AES cipher using the key
+	block, err := aes.NewCipher(keyBytes)
+	if err != nil {
+		return "", err
+	}
+
+	// Generate a new random IV
+	iv := make([]byte, aes.BlockSize)
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+
+	// Encrypt the text using the AES cipher and IV
+	stream := cipher.NewCTR(block, iv)
+	stream.XORKeyStream(textBytes, textBytes)
+
+	// Combine the IV and encrypted text into a single string
+	encrypted := append(iv, textBytes...)
+	return base64.URLEncoding.EncodeToString(encrypted), nil
+}
+
+func DecryptString(key, text string) (string, error) {
+	// Convert key to 16 bytes
+	keyBytes := []byte(key)
+	if len(keyBytes) != 16 {
+		return "", errors.New("key must be 16 bytes")
+	}
+
+	// Convert text to bytes
+	textBytes, err := base64.URLEncoding.DecodeString(text)
+	if err != nil {
+		return "", err
+	}
+
+	// Generate a new AES cipher using the key
+	block, err := aes.NewCipher(keyBytes)
+	if err != nil {
+		return "", err
+	}
+
+	// Separate the IV and encrypted text
+	iv := textBytes[:aes.BlockSize]
+	textBytes = textBytes[aes.BlockSize:]
+
+	// Decrypt the text using the AES cipher and IV
+	stream := cipher.NewCTR(block, iv)
+	stream.XORKeyStream(textBytes, textBytes)
+
+	return string(textBytes), nil
 }
